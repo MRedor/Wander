9391a879a6faa5bb642603d4aa9304fb9971df7b
Maria Palchenkova <mashapalchenkova@gmail.com>
2020-01-10 14:07:48 +0300

    update todo

--- src/controllers/server.go
+++ src/controllers/server.go
@@ -11,13 +11,13 @@ func StartServer() {
 
 	e.GET("/api/objects/:id", getObjectById)
 	e.GET("/api/objects/getFeatured/:boundingBox", getRandomObjects)
+
 	e.POST("/api/routes/get", getRoute)
 	e.GET("/api/routes/:id", getRouteById)
 	e.POST("/api/routes/removePoint", removePoint)
 
 	e.POST("/api/feedback", saveFeedback)
 
-	//TODO:
 	e.GET("/api/list/:id", getListById)
 	e.GET("/api/lists", getLists)
 
